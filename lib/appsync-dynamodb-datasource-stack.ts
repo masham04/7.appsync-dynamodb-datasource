d8f970404a81f5b75ad903ff558476d9726152b5
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-22 10:05:14 +0500

    new

--- lib/appsync-dynamodb-datasource-stack.ts
+++ lib/appsync-dynamodb-datasource-stack.ts
@@ -1,9 +1,45 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as ddb from "@aws-cdk/aws-dynamodb";
 
 export class AppsyncDynamodbDatasourceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, "GRAPHQL_API", {
+      name: "cdk-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.gql"), ///Path specified for lambda
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY, ///Defining Authorization Type
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)), ///set expiration for API Key
+          },
+        },
+      },
+      xrayEnabled: true, ///Enables xray debugging
+    });
+
+    new cdk.CfnOutput(this, "APIGraphQlURL", {
+      value: api.graphqlUrl,
+    });
+
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
+
+    const dynamoDBTable = new ddb.Table(this, "Table", {
+      partitionKey: {
+        name: "id",
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+    const db_data_source = api.addDynamoDbDataSource(
+      "DataSources",
+      dynamoDBTable
+    );
+
+    
   }
 }
